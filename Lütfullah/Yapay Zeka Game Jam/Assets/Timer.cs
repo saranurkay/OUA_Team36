147d354a81f403e08e1ecd44fa0b0f05aec4f82e
Alperen Türkmen <57950250+AlperenTurkmen@users.noreply.github.com>
2024-05-19 18:41:45 +0300

    Nolur calissin ya
    assetsi replace ettim

---
+++ Lütfullah/Yapay Zeka Game Jam/Assets/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    void Update()
+    {
+        if( remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+
+        if (remainingTime < 60)
+        {
+            Debug.Log(remainingTime);
+            timerText.color = Color.red;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+        
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
